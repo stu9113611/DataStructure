a837e197bdfb369bbe5abca6b85994908be75e03
Ian Cai <60545226+stu9113611@users.noreply.github.com>
2023-11-22 00:08:25 +0800

    Change naming style

--- Src/main.c
+++ Src/main.c
@@ -7,39 +7,39 @@
 int main(void)
 {
     while (1) {
-        IntList list = IntList_create();
+        IntList list = IntList_Create();
         int nums[3] = { 0, 1, 2 };
-        IntList_addWithArr(&list, nums, 3);
-        IntList_add(&list, 3);
-        IntList_print(list);
+        IntList_AddWithArr(&list, nums, 3);
+        IntList_Add(&list, 3);
+        IntList_Print(list);
 
-        printf("%d %d %d %d\n", IntList_get(&list, 0), IntList_get(&list, 1), IntList_get(&list, 2), IntList_get(&list, 3));
+        printf("%d %d %d %d\n", IntList_Get(&list, 0), IntList_Get(&list, 1), IntList_Get(&list, 2), IntList_Get(&list, 3));
 
-        IntList_insert(&list, 3, 4);
-        IntList_print(list);
+        IntList_Insert(&list, 3, 4);
+        IntList_Print(list);
 
-        IntList_insert(&list, 1, 5);
-        IntList_print(list);
+        IntList_Insert(&list, 1, 5);
+        IntList_Print(list);
 
-        IntList_remove(&list, 2);
-        IntList_print(list);
+        IntList_Remove(&list, 2);
+        IntList_Print(list);
 
-        IntList list2 = IntList_create();
-        IntList_add(&list2, 2);
-        IntList_add(&list2, 3);
-        IntList_print(list2);
+        IntList list2 = IntList_Create();
+        IntList_Add(&list2, 2);
+        IntList_Add(&list2, 3);
+        IntList_Print(list2);
 
-        IntList_copy(&list, &list2);
+        IntList_Copy(&list, &list2);
 
-        IntList_print(list2);
+        IntList_Print(list2);
 
         int arr[10] = { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 };
-        IntList list3 = IntList_createWithArray(arr, 10);
-        IntList_print(list3);
+        IntList list3 = IntList_CreateWithArray(arr, 10);
+        IntList_Print(list3);
 
-        IntList_destroy(&list);
-        IntList_destroy(&list2);
-        IntList_destroy(&list3);
+        IntList_Destroy(&list);
+        IntList_Destroy(&list2);
+        IntList_Destroy(&list3);
     }
 
     
