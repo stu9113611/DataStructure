1212b24523c1fc25e83ee1b9321b12f4566f0381
Ian Cai <60545226+stu9113611@users.noreply.github.com>
2023-11-22 00:05:35 +0800

    Change naming style

--- Src/List.h
+++ Src/List.h
@@ -5,38 +5,38 @@
 
 struct IntNode
 {
-    int value;
-    struct IntNode* next;
+	int value;
+	struct IntNode* next;
 };
 // Basic unit of an IntList.
 typedef struct IntNode IntNode;
 
 // Allocate an IntNode, which needs to be freed explicitly.
-IntNode* IntNode_createRef(int value);
+IntNode* IntNode_CreateRef(int value);
 
 // Print an IntNode in details.
-void IntNode_print(IntNode* p_node);
+void IntNode_Print(IntNode* p_node);
 
 // ----------------------------- IntList ---------------------------------
 
 struct IntList {
-    IntNode* head;
-    unsigned int size;
+	IntNode* head;
+	unsigned int size;
 };
 // A list which only contains integers.
 typedef struct IntList IntList;
 
-IntList     IntList_create();
-IntList     IntList_createWithArray(const int* arr, unsigned int size);
-void        IntList_destroy(IntList* p_list);
-bool        IntList_isEmpty(IntList list);
-void        IntList_print(IntList list);
-int         IntList_get(const IntList* p_list, unsigned int index);
-void        IntList_add(IntList* p_list, int value);
-void        IntList_addWithArr(IntList* p_list, const int* arr, unsigned int size);
-void        IntList_insert(IntList* p_list, unsigned int index, int value);
-void        IntList_remove(IntList* p_list, unsigned int index);
-void        IntList_copy(IntList* p_from, IntList* p_to);
+IntList     IntList_Create();
+IntList     IntList_CreateWithArray(const int* arr, unsigned int size);
+void        IntList_Destroy(IntList* p_list);
+bool        IntList_IsEmpty(IntList list);
+void        IntList_Print(IntList list);
+int         IntList_Get(const IntList* p_list, unsigned int index);
+void        IntList_Add(IntList* p_list, int value);
+void        IntList_AddWithArr(IntList* p_list, const int* arr, unsigned int size);
+void        IntList_Insert(IntList* p_list, unsigned int index, int value);
+void        IntList_Remove(IntList* p_list, unsigned int index);
+void        IntList_Copy(IntList* p_from, IntList* p_to);
 
-IntNode*    IntList_getRef(const IntList* p_list, unsigned int index);
-IntNode*    IntList_getLastRef(const IntList* p_list);
+IntNode* IntList_GetRef(const IntList* p_list, unsigned int index);
+IntNode* IntList_GetLastRef(const IntList* p_list);
